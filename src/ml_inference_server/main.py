8d1071efb2ab2e9c7121eec05e012e638f807f75
PSRahul <rahulpsr95@gmail.com>
2026-09-23 16:40:46 +0530

    add inference function

--- src/ml_inference_server/main.py
+++ src/ml_inference_server/main.py
@@ -1,6 +1,7 @@
+import asyncio
 import uuid
 
-from fastapi import FastAPI, HTTPException
+from fastapi import BackgroundTasks, FastAPI, HTTPException
 
 from src.ml_inference_server.models import AnalyzeRequest, JobResponse
 
@@ -9,18 +10,32 @@ app = FastAPI()
 jobs: dict[str, JobResponse] = {}
 
 
+async def run_inference(job_response: JobResponse):
+    job_response.status = "running"
+    job_response.progress = 25
+    await asyncio.sleep(5)
+    job_response.status = "completed"
+    job_response.progress = 100
+
+
 @app.get(path="/health")
 async def get_health():
     return {"status": "ok"}
 
 
-@app.post(path="/analyze", response_model=JobResponse)
-async def post_analyze(analyze_request: AnalyzeRequest):
+@app.post(
+    path="/analyze",
+    response_model=JobResponse,
+)
+async def post_analyze(
+    analyze_request: AnalyzeRequest, background_tasks: BackgroundTasks
+):
 
     response = JobResponse(
         job_id=str(uuid.uuid4()),
         status="pending",
     )
+    background_tasks.add_task(run_inference, response)
     jobs[response.job_id] = response
     return response
 
