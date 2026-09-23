f9fdae5e845feb828adc3f8178533764b0e207ed
PSRahul <rahulpsr95@gmail.com>
2026-09-23 11:52:11 +0530

    add /analyze and /jobs

--- src/ml_inference_server/main.py
+++ src/ml_inference_server/main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI
+import uuid
 
-from src.ml_inference_server.models import AnalyzeResult, JobResponse
+from fastapi import FastAPI, HTTPException
 
-_, _ = AnalyzeResult, JobResponse
+from src.ml_inference_server.models import AnalyzeRequest, JobResponse
 
 app = FastAPI()
+
+jobs: dict[str, JobResponse] = {}
 
 
 @app.get(path="/health")
 async def get_health():
     return {"status": "ok"}
+
+
+@app.post(path="/analyze", response_model=JobResponse)
+async def post_analyze(analyze_request: AnalyzeRequest):
+    response = JobResponse(
+        job_id=str(uuid.uuid4()),
+        status="pending",
+    )
+    jobs[response.job_id] = response
+    return response
+
+
+@app.get(path="/jobs/{job_id}", response_model=JobResponse)
+async def get_jobs(job_id: str):
+    if job_id in jobs:
+        return jobs[job_id]
+    else:
+        raise HTTPException(status_code=404, detail="Job ID not found")
